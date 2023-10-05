8b97eac778a5f392b0278a7f867e2584871d7bd5
122344AS <143315452+122344AS@users.noreply.github.com>
2023-10-05 13:48:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/1238muj/THALAPATHY-FILTER-BOT.git /THALAPATHY-FILTER-BOT 
+  git clone https://github.com/122344AS/Doraemonmoviebot.git /Doraemonmoviebot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /THALAPATHY-FILTER-BOT 
+  git clone $UPSTREAM_REPO /Doraemonmoviebot 
 fi
-cd /THALAPATHY-FILTER-BOT 
+cd /Doraemonmoviebot 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
