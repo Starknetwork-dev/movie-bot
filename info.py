9652bfd9adbf1c0f2cf37616930639109a8ee3e5
122344AS <143315452+122344AS@users.noreply.github.com>
2023-10-28 12:13:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -105,8 +105,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://doraemonmkvl-9f4090e515c2.herokuapp.com".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://doraemonmkvl-9f4090e515c2.herokuapp.com".format(FQDN, PORT)
+URL = "https://akka.onrender.com".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://akka.onrender.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -121,9 +121,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://doraemonmkvl-9f4090e515c2.herokuapp.com".format(FQDN)
+    URL = "https://akka.onrender.com/".format(FQDN)
 else:
-    URL = "https://doraemonmkvl-9f4090e515c2.herokuapp.com".format(FQDN)
+    URL = "https://akka.onrender.com/".format(FQDN)
 
 
 
